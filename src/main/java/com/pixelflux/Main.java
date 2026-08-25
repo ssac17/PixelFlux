99fa70fe206800ea1bdac9ac7727031d29f9d943
ssac17 <84308365+ssac17@users.noreply.github.com>
2026-08-25 13:42:27 +0900

    닫기 단축키 추가

--- src/main/java/com/pixelflux/Main.java
+++ src/main/java/com/pixelflux/Main.java
@@ -3,8 +3,12 @@ package com.pixelflux;
 import com.pixelflux.controller.MainController;
 import com.pixelflux.view.MainView;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,8 +18,16 @@ public class Main extends Application {
         Parent root = mainView.createContent();
         new MainController(mainView);
 
+
+
         //창 크기
         Scene scene = new Scene(root, 800, 700);
+        KeyCombination closeShortcut = new KeyCodeCombination(KeyCode.W, KeyCombination.SHORTCUT_DOWN);
+        //command + w, 창 닫기
+        scene.getAccelerators().put(closeShortcut, () -> {
+            Platform.exit();
+            System.exit(0);
+        });
         primaryStage.setTitle("Pixelflux");
         primaryStage.setScene(scene);
 
